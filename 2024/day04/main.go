f13daa740f6da2a5a89be63800da70df1974a7d1
Zokiio <Joakim.hall@zottik.com>
2024-12-04 22:31:08 +0100

    comments

--- 2024/day04/main.go
+++ 2024/day04/main.go
@@ -24,61 +24,65 @@ func main() {
 	fmt.Println("Output:", part2Answer)
 }
 
-func part1(input string) int {
-	countXmas := 0
+func parseInput(input string) [][]rune {
 	var inputRunes [][]rune
-
 	splitString := strings.Split(strings.TrimSpace(strings.ReplaceAll(input, "\r\n", "\n")), "\n")
 	for _, s := range splitString {
 		inputRunes = append(inputRunes, []rune(s))
 	}
+	return inputRunes
+}
+
+func part1(input string) int {
+	countXmas := 0
+	inputRunes := parseInput(input)
 
 	for i, row := range inputRunes {
 		for j := 0; j < len(row); j++ {
-			// hor
+			// ➡️
 			if j <= (len(row) - 4) {
 				if row[j] == 'X' && row[j+1] == 'M' && row[j+2] == 'A' && row[j+3] == 'S' {
 					countXmas++
 				}
 			}
-			// rev hor
+			// ⬅️
 			if j >= 3 {
 				if row[j] == 'X' && row[j-1] == 'M' && row[j-2] == 'A' && row[j-3] == 'S' {
 					countXmas++
 				}
 			}
-			// up
+			// ⬆️
 			if i >= 3 {
 				if row[j] == 'X' && inputRunes[i-1][j] == 'M' && inputRunes[i-2][j] == 'A' && inputRunes[i-3][j] == 'S' {
 					countXmas++
 				}
 			}
-			// diagonal upwards right
+			// ↗️
 			if i >= 3 && j <= (len(row)-4) {
 				if row[j] == 'X' && inputRunes[i-1][j+1] == 'M' && inputRunes[i-2][j+2] == 'A' && inputRunes[i-3][j+3] == 'S' {
 					countXmas++
 				}
 			}
-			// diagonal upwards left
+			// ↖️
 			if i >= 3 && j >= 3 {
 				if row[j] == 'X' && inputRunes[i-1][j-1] == 'M' && inputRunes[i-2][j-2] == 'A' && inputRunes[i-3][j-3] == 'S' {
 					countXmas++
 				}
 			}
 
-			// down
+			// ⬇️
 			if i <= (len(inputRunes) - 4) {
 				if row[j] == 'X' && inputRunes[i+1][j] == 'M' && inputRunes[i+2][j] == 'A' && inputRunes[i+3][j] == 'S' {
 					countXmas++
 				}
 			}
-			// diagonal downwards right
+			// ↘️
 			if i <= (len(inputRunes)-4) && j <= (len(row)-4) {
 				if row[j] == 'X' && inputRunes[i+1][j+1] == 'M' && inputRunes[i+2][j+2] == 'A' && inputRunes[i+3][j+3] == 'S' {
 					countXmas++
 				}
 			}
-			// diagonal downwards left
+			// ↙️
 			if i <= (len(inputRunes)-4) && j >= 3 {
 				if row[j] == 'X' && inputRunes[i+1][j-1] == 'M' && inputRunes[i+2][j-2] == 'A' && inputRunes[i+3][j-3] == 'S' {
 					countXmas++
@@ -91,12 +95,7 @@ func part1(input string) int {
 
 func part2(input string) int {
 	countXmas := 0
-	var inputRunes [][]rune
-
-	splitString := strings.Split(strings.TrimSpace(strings.ReplaceAll(input, "\r\n", "\n")), "\n")
-	for _, s := range splitString {
-		inputRunes = append(inputRunes, []rune(s))
-	}
+	inputRunes := parseInput(input)
 
 	for i, row := range inputRunes {
 		if i < 1 || i >= len(inputRunes)-1 {
